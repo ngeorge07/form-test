6e7f848dcf10a80f4ccd30cea973f2743ab04c20
George <ngeorge07@yahoo.com>
2022-01-11 20:55:51 +0200

    working calendar

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+let url =
+  "https://usebasin.com/api/v1/submissions?form_id=ab2834d7e27b&api_token=1cbd5d2e93116be3389923b4e8c5fd5d";
+
+const options = {
+  headers: {
+    "x-apikey": "61b64c7672a03f5dae822307",
+  },
+};
+
 function getFormData() {
-  let url =
-    "https://usebasin.com/api/v1/submissions?form_id=ab2834d7e27b&api_token=1cbd5d2e93116be3389923b4e8c5fd5d";
-
-  const options = {
-    headers: {
-      "x-apikey": "61b64c7672a03f5dae822307",
-    },
-  };
-
-  let data = fetch(url, options)
+  fetch(url, options)
     .then((response) => response.json())
     .then(updateForm);
+}
+
+function getHours() {
+  fetch(url, options)
+    .then((response) => response.json())
+    .then(checkHoursAvailable);
+}
+
+function showHours() {
+  fetch(url, options)
+    .then((response) => response.json())
+    .then(showAvailableHours);
 }
 
 let today = new Date();
@@ -25,23 +37,77 @@ const mmLast = String(today.getMonth() + 2).padStart(2, "0");
 today = `${dd}-${mm}-${yyyy}`;
 lastDay = `${dd}-${mmLast}-${yyyy}`;
 
-let unavailableDays = [];
+let datepicker;
 
 function updateForm(a) {
-  a.submissions.forEach((e) => {
-    unavailableDays.push(e.payload_params.date);
-  });
-  console.log(unavailableDays);
+  // a.submissions.forEach((e) => {
+  //   unavailableDays.push(e.payload_params.date);
+  // });
+  // console.log(unavailableDays);
   const elem = document.getElementById("date");
-  const datepicker = new Datepicker(elem, {
+  datepicker = new Datepicker(elem, {
     buttonClass: "btn",
     minDate: today,
     maxDate: lastDay,
     daysOfWeekDisabled: [0, 6],
     format: "dd/mm/yyyy",
     autohide: true,
-    datesDisabled: unavailableDays,
+    // datesDisabled: ["28/01/2022"],
   });
+}
+
+function getOccurrence(array, value) {
+  let count = 0;
+  array.forEach((v) => v === value && count++);
+  return count;
+}
+
+function checkHoursAvailable(a) {
+  const submissions = a.submissions;
+  let dates = [];
+  let unavailableDays = [];
+
+  for (let i = 0; i < submissions.length; i++) {
+    dates.push(submissions[i].payload_params.date);
+    if (getOccurrence(dates, submissions[i].payload_params.date) === 3) {
+      unavailableDays.push(submissions[i].payload_params.date);
+      datepicker.setOptions({ datesDisabled: unavailableDays });
+    }
+  }
+}
+
+function showAvailableHours(a) {
+  const hSubmissions = a.submissions;
+  const selectedDay = document.querySelector("#date").value;
+  let hours = [];
+
+  const temp = document.querySelector("template").content;
+  const clone = temp.cloneNode(true);
+
+  for (let i = 0; i < hSubmissions.length; i++) {
+    hours.push(hSubmissions[i].payload_params.time);
+
+    if (hSubmissions[i].payload_params.date === selectedDay) {
+      console.log("ana");
+
+      if (hSubmissions[i].payload_params.time === "11:00") {
+        clone.querySelector("#container-1100").remove();
+      }
+
+      if (hSubmissions[i].payload_params.time === "12:00") {
+        clone.querySelector("#container-1200").remove();
+      }
+
+      if (hSubmissions[i].payload_params.time === "13:00") {
+        clone.querySelector("#container-1300").remove();
+      }
+    }
+  }
+
+  document.querySelector("#time").appendChild(clone);
+
+  console.log(selectedDay);
+  console.log(hSubmissions);
 }
 
 function makeDayClick() {
@@ -53,9 +119,14 @@ function makeDayClick() {
     ".datepicker-cell:not(.disabled)"
   );
 
+  getHours();
+
   function dayIsClicked() {
     let t = 0;
-    console.log(t);
+
+    if (document.querySelector("#time-options")) {
+      document.querySelector("#time-options").remove();
+    }
 
     availableDays.forEach((d) => {
       d.removeEventListener("click", dayIsClicked);
@@ -63,6 +134,15 @@ function makeDayClick() {
 
     nextButton.removeEventListener("click", nextClicked);
     prevButton.removeEventListener("click", nextClicked);
+
+    document.querySelector("#submit").classList.remove("hidden");
+    document.querySelector("#time").classList.remove("hidden");
+
+    if (document.querySelector("#time-options")) {
+      document.querySelector("#time-options").remove();
+    }
+
+    showHours();
   }
 
   availableDays.forEach((d) => {
@@ -83,8 +163,6 @@ function makeDayClick() {
     availableDays.forEach((d) => {
       d.addEventListener("click", dayIsClicked);
     });
-
-    console.log(availableDays);
   }
 
   nextButton.addEventListener("click", nextClicked);
